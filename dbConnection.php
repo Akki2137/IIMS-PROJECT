f3ee78662adba325442527b1461d70afa0d03b22
akki2137 <akshayraut960.dj@gmail.com>
2023-08-03 18:26:21 +0530

    changes in compose file

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,5 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('localhost','admin','admin','iims-db')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('localhost','root','abcd1234','Project')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
